1d793cec3615e7aaed54f2a6fb246c0acc3c4c25
glav-git <71907036+glav-git@users.noreply.github.com>
2023-05-15 04:15:52 +0300

    add find_index function

--- find_in_list.py
+++ find_in_list.py
@@ -5,3 +5,7 @@ T = TypeVar('T')
 
 def find_first(i: Iterable[T], f: Callable[[T], bool]) -> T | None:
     return next(iter(o for o in i if f(o)), None)
+
+
+def find_index(i: Iterable[T], f: Callable[[T], bool]) -> int | None:
+    return next(iter(indx for indx, o in enumerate(i) if f(o)), None)
